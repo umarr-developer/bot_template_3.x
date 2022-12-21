96632575528227c8f1db37728b4b87d9ea38ff41
umarr-developer <umarr.developer@gmail.com>
2022-12-21 07:32:51 +0600

    edit User model method

--- src/models/user.py
+++ src/models/user.py
@@ -14,7 +14,7 @@ class User(Base):
     update_on = Column(Date, default=datetime.now, onupdate=datetime.now)
 
     @classmethod
-    async def new(cls, db_session: sessionmaker, user_id: int) -> 'User':
+    async def new(cls, db_session: sessionmaker, user_id: int) -> tuple['User']:
         sql = insert(cls).values(user_id=user_id)
         async with db_session() as session:
             await session.execute(sql)
@@ -22,7 +22,7 @@ class User(Base):
         return await cls.get(db_session, user_id=user_id)
 
     @classmethod
-    async def get(cls, db_session: sessionmaker, user_id: int) -> 'User':
+    async def get(cls, db_session: sessionmaker, user_id: int) -> tuple['User']:
         sql = select(cls).where(cls.user_id == user_id)
         async with db_session() as session:
             response = await session.execute(sql)
